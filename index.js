461a911778213182cc679a61ea58564c04445c35
Leyla K <l.karamad@gmail.com>
2022-06-02 11:30:32 +0200

    js code edited

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ buttons.map( button => {
                     display.innerText = "Error"
                 }
                 break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
+            
             default:
                 display.innerText += e.target.innerText;
         }
